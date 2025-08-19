312d5352c3e89387cc25604e6bdb4afeeab76f46
Ziad Elnagar <zelnagar@uwo.ca>
2025-08-19 14:22:18 -0400

    Build for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,8 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PersonalPortfolio/',               // <-- repo name
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+  build: { outDir: 'docs' }                  // <-- build to /docs for GH Pages
 });
