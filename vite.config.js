a7872315e2a2ceb9deeeb009f993324a2283786f
Ziad Elnagar <zelnagar@uwo.ca>
2025-08-19 14:52:58 -0400

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/PersonalPortfolio/',               // <-- repo name
+  base: "/PersonalPortfolio",
   plugins: [react(), tailwindcss()],
   resolve: { alias: { '@': path.resolve(__dirname, './src') } },
-  build: { outDir: 'docs' }                  // <-- build to /docs for GH Pages
-});
+})
