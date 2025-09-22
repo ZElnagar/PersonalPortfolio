8739c94b35728dca042dc518e0e0421028272ce6
Ziad Elnagar <zelnagar@uwo.ca>
2025-09-22 00:57:27 -0400

    Clean rebuild

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: "./",          // <-- trailing slash!
+  base: "./",
   build: { outDir: "docs" },
   plugins: [react(), tailwindcss()],
   resolve: { alias: { '@': path.resolve(__dirname, './src') } },
