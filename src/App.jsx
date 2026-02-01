a269ea57111c3ad0a16d521017e28fd9c69b3bfd
Ziad Elnagar <zelnagar@uwo.ca>
2026-01-31 21:48:14 -0500

    Resume update, Analytics Enabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
